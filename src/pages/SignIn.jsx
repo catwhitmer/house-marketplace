6b57d3a66a63639963ad14bad8d2c77c808a46fb
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2022-12-30 13:52:34 -0500

    added sign in form

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,7 +6,7 @@ import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 function SignIn() {
   const navigate = useNavigate();
 
-  const [showPassword, setShowPasword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -14,12 +14,65 @@ function SignIn() {
 
   const { email, password } = formData;
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
+
+        <form>
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input
+              className="passwordInput"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              className="showPassword"
+              src={visibilityIcon}
+              alt="show password"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link className="forgotPasswordLink" to="/forgot-password">
+            Forgot Password
+          </Link>
+
+          <div className="signInBar">
+            <p signInText>Sign In</p>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
+        {/* Google OAuth */}
+
+        <Link className="registerLink" to="/sign-up">
+          Sign Up Instead
+        </Link>
       </div>
     </>
   );
