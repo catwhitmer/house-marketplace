4b4089625c36dd4b75a378e1b790b179ceea25c6
Cat Whitmer <60889037+catwhitmer@users.noreply.github.com>
2023-01-13 13:55:43 -0500

    created useAuthStatus hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,28 @@
+import { useEffect, useState, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useAuthStatus = () => {
+  const isMounted = useRef(true);
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth();
+
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setLoading(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return { loggedIn, loading };
+};
